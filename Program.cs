a3b4e9d40eecf40bf9114e37961db8d2c7ea6c64
bakinseye <bakinseye247@gmail.com>
2023-12-01 09:57:23 +0100

    Initial commt

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿
+namespace ClassExcercise {
+    class Program
+    {
+        public static void Main()
+        {
+            double rate = 0.055;
+            float principal = 100;
+            short time = 4;
+            short numberCompounded = 1;
+
+            CompoundInterest.CalculateInterestAndAmount(rate, principal, time, numberCompounded);
+
+           
+        }
+
+
+        class  CompoundInterest
+        {
+            public static void CalculateInterestAndAmount(double rate, float principal, short time, short numberCompounded)
+
+            {
+                decimal amount = (decimal)(principal * Math.Pow(1 + rate / numberCompounded, time  * numberCompounded));
+
+                decimal compoundInterest = amount - (decimal)principal;
+                
+                Console.WriteLine("Amount is equal to " + amount);
+
+
+                Console.WriteLine("The compound interest is " + compoundInterest);
+
+                
+            }
+        }
+    }
+
+
+
+    }
+
+
+
+
+
+
+
+
